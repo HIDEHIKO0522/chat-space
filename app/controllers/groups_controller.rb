0cb1df8b55e4c907b3e9694124578e0df21b0f44
HIDEHIKO SHIRAIWA <sirap@jcom.home.ne.jp>
2019-11-28 00:21:34 +0900

    グループ投稿機能の微修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,40 +1,40 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
- def new
-  @group = Group.new
-  @group.users << current_user
- end
+  def index
+  end
+ 
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
  
   def create
     @group = Group.new(group_params)
-      if @group.save
-        redirect_to root_path, notice: 'グループを作成しました'
-      else
-        render :new
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
     end
   end
- 
- def index
- end
 
- def edit
+  def edit
   # @group = Group.find(params[:id]) ///set_tweetでまとめる
- end
+  end
  
- def update
+  def update
   # @group = Group.find(params[:id]) ///set_tweetでまとめる
-  if @group.update(group_params)
-    redirect_to root_path, notice: 'グループを更新しました'
-  else
-    render :edit
-  end  
- end
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end  
+  end
   
- private
-  def group_params 
+  private
+  def group_params
     params.require(:group).permit(:name, user_ids: [] )
-  end  
+  end
 
   def set_group
     @group = Group.find(params[:id])
